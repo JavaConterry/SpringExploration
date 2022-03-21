9ed59022e44e8736174e8735fa564a87c7ad31e6
JavaConterry <conterry05@gmail.com>
2022-03-21 23:46:31 +0200

    WIP

--- src/main/java/com/example/sweater/WebController.java
+++ src/main/java/com/example/sweater/WebController.java
@@ -1,23 +1,33 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class WebController {
+    @Autowired
+    private MessageRepository messageRepository; 
+
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Model model) {
-        model.addAttribute("name", name);
+                           Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
     }
 
     @GetMapping
-    public String main(Model model){
-        model.addAttribute("some", "its here without tymleaf");
+    public String main(Map<String, Object> model){
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
         return "main";
     }
 
