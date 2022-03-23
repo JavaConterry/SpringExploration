42611c73a1d0a981d74fcc00c2ad1d86e38ecb39
JavaConterry <conterry05@gmail.com>
2022-03-23 13:09:41 +0200

    fixed

--- src/main/java/com/example/sweater/WebController.java
+++ src/main/java/com/example/sweater/WebController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -14,16 +15,17 @@ import java.util.Map;
 @Controller
 public class WebController {
     @Autowired
-    private MessageRepository messageRepository; 
+    private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
+    @GetMapping(path="/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
                            Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
 
-    @GetMapping()
+
+    @GetMapping(path="/")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepository.findAll();
 
