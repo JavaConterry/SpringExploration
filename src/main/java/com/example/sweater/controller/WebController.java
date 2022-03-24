32eec2e3f05ba29a51f1e1c7042d75d17588704f
JavaConterry <conterry05@gmail.com>
2022-03-24 23:23:13 +0200

    WIP

--- src/main/java/com/example/sweater/controller/WebController.java
+++ src/main/java/com/example/sweater/controller/WebController.java
@@ -1,13 +1,11 @@
-package com.example.sweater;
+package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
 import com.example.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -18,15 +16,12 @@ public class WebController {
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping(path="/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-
-    @GetMapping(path="/")
+    @GetMapping(path="/main")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepository.findAll();
 
@@ -34,7 +29,7 @@ public class WebController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping(path="/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message = new Message(text, tag);
         messageRepository.save(message);
